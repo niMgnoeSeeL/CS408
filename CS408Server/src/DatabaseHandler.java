d535b1cf3b3e08bbd546ac3c94ee1417057a4a3f
niMgnoeSeeL <nim.gnoes.eel@gmail.com>
2015-11-25 18:02:17 +0900

    seongminLee: change some errors, add database initializing

--- CS408Server/src/DatabaseHandler.java
+++ CS408Server/src/DatabaseHandler.java
@@ -12,7 +12,7 @@ import java.text.SimpleDateFormat;
  */
 public class DatabaseHandler {
 	private static final String PATH = "path";
-	private static final String CON = "jdbc:mysql://localhost";
+	private static final String CON = "jdbc:mysql://localhost/able";
 	private static final String USER = "root";
 	private static final String PASS = "bionic24";
 
@@ -31,15 +31,13 @@ public class DatabaseHandler {
 		}
 	}
 
-	public void addTimelog(int user, int booth) {
+	public void addTimelog(String string, String string2) {
 		try {
 			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 			String currentTime = sdf.format(new Date());
-			String query = "UPDATE timelog SET endtime=" + currentTime
-					+ " WHERE endTime IS NULL AND user =" + user;
+			String query = "UPDATE timelog SET endtime=" + currentTime + " WHERE endTime IS NULL AND user =" + string;
 			statement.executeUpdate(query);
-			query = "INSERT INTO timelog VALUES(" + user + "," + booth + ","
-					+ currentTime + ",NULL)";
+			query = "INSERT INTO timelog VALUES(" + string + "," + string2 + "," + currentTime + ",NULL)";
 			statement.executeUpdate(query);
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -48,6 +46,7 @@ public class DatabaseHandler {
 
 	/**
 	 * The top 3 most popular booths
+	 * 
 	 * @returns ResultSet, the retrieved data from the database
 	 */
 	public ResultSet getPopular() {
@@ -58,23 +57,28 @@ public class DatabaseHandler {
 			return statement.executeQuery(query);
 		} catch (SQLException e) {
 			e.printStackTrace();
+			return null;
 		}
 	}
-	
+
 	/**
-	 * Simple recommendations for users based on popular booths which hasn't been visited yet
-	 * @param String user, the user which will get the recommendation
+	 * Simple recommendations for users based on popular booths which hasn't
+	 * been visited yet
+	 * 
+	 * @param String
+	 *            user, the user which will get the recommendation
 	 * @returns ResultSet, the retrieved data from the database
 	 */
 	public ResultSet getRecommended(String user) {
 		try {
-			String query = "SELECT booth.name FROM booth, timelog WHERE booth.id = timelog.booth AND " 
-					+ "booth.id NOT IN (SELECT booth FROM timelog WHERE userID="+user+") GROUP BY "
+			String query = "SELECT booth.name FROM booth, timelog WHERE booth.id = timelog.booth AND "
+					+ "booth.id NOT IN (SELECT booth FROM timelog WHERE userID=" + user + ") GROUP BY "
 					+ "booth.id ORDER BY SUM(TIMESTAMPDIFF(MINUTE,timelog.startTime,timelog.endTime)) "
 					+ "DESC LIMIT 3";
 			return statement.executeQuery(query);
 		} catch (SQLException e) {
 			e.printStackTrace();
+			return null;
 		}
 	}
 }
--- CS408Server/src/GableServer.java
+++ CS408Server/src/GableServer.java
@@ -1,21 +1,55 @@
 import java.net.*;
-import java.io.*;
+import java.sql.*;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.jdbc.ScriptRunner;
+
+import java.io.*;
 
 public class GableServer {
 	private static int connectionPort = 4711;
-    
-	public static void main(String[] args) {  
+	private static final String PATH = "path";
+	private static final String CON = "jdbc:mysql://localhost/CS408";
+	private static final String USER = "root";
+	private static final String PASS = "bionic24";
+
+	public static void main(String[] args) {
 		try {
-			ServerSocket serverSocket = new ServerSocket(connectionPort); 
+			ServerSocket serverSocket = new ServerSocket(connectionPort);
 			System.out.println("Server started listening on port: " + connectionPort);
+			settingDB();
 			while (true) {
 				new RequestChecker(serverSocket.accept()).start();
 			}
-			//serverSocket.close();
+			// serverSocket.close();
 		} catch (IOException e) {
-      System.err.println("Could not listen on port: " + connectionPort);
-      System.exit(1);
-    }
-  }
+			System.err.println("Could not listen on port: " + connectionPort);
+			System.exit(1);
+		}
+	}
+	
+	public static void settingDB(){
+		Connection conn;
+		try {
+			conn = DriverManager.getConnection(CON, USER, PASS);
+			ScriptRunner runner = new ScriptRunner(conn);
+			Reader reader = Resources.getResourceAsReader("db.sql");
+//			runner.setDelimiter("]");
+			runner.setLogWriter(null);
+			runner.setErrorLogWriter(null);
+			runner.runScript(reader);
+			conn.commit();
+			reader.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 }
